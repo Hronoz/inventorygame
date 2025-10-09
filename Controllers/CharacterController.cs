4de19da5c840a0b132aa5d567be6f57ffd4ab936
Hronoz <hronozovichhronoz@gmail.com>
2025-10-09 18:37:00 +0300

    Added new CharacterController actions Added GiveItem action Added EquipItem action Added UnequipItem action

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -13,20 +13,49 @@ namespace InventoryGame.Controllers
             _characterService = characterService;
         }
 
-        [HttpGet("{id}")]
-        public ActionResult<Character> GetCharacter(int id)
-        {
-            Character character = _characterService.GetCharacter(id);
-
-            return character;
-        }
-
         [HttpPost]
         public ActionResult CreateCharacter(Character character)
         {
             _characterService.CreateCharacter(character);
 
             return CreatedAtAction(nameof(GetCharacter), new { name = character.Name }, character);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetCharacter(int id)
+        {
+            Character character = _characterService.GetCharacter(id);
+
+            if (character is null)
+            {
+                return NotFound();
+            }
+
+            return character;
+        }
+
+        [HttpPost("item/{id}")]
+        public ActionResult GiveItem(int id, Item item)
+        {
+            _characterService.GiveItem(id, item);
+
+            return NotFound();
+        }
+
+        [HttpPost("equip/{id}")]
+        public ActionResult EquipItem(int id, Item item)
+        {
+            _characterService.EquipItem(id, item);
+
+            return Ok();
+        }
+
+        [HttpPost("unequip/{id}")]
+        public ActionResult UnequipItem(int id, ItemType slotType)
+        {
+            _characterService.UnequipItem(id, slotType);
+
+            return Ok();
         }
     }
 }
